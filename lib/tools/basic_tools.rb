cebc287cb4a5593103be4bdd7e3267abc9264118
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-08-19 15:45:55 +0700

    lint and add top level comment

--- lib/tools/basic_tools.rb
+++ lib/tools/basic_tools.rb
@@ -1,41 +1,31 @@
-
+# reusable tool for debuging,gameplay and adding chess pieces on board
 module Basic_tools
-  def select_grid(user_grid_select,grid_location = [1,1],square = @board,&block)
-      #grid_location = [1,1]
+  def select_grid(user_grid_select, grid_location = [1, 1], square = @board, &block)
+    starting_square_y = square
 
-      starting_square_y = square
+    loop do
+      return block.call(square) if grid_location == user_grid_select
 
-      loop do
-          if grid_location == user_grid_select
-              #return square.data
-              #return yield(square)
-              return block.call(square)
-          end
+      break if square.right.nil?
 
-          if square.right.nil?
-              break
-          end
+      next_square_x = square.right
+      square = next_square_x
+      grid_location[1] += 1
+    end
+    grid_location[1] = 1
+    grid_location[0] += 1
+    return nil if grid_location[0] > user_grid_select[0]
 
-          next_square_x = square.right
-          square = next_square_x
-          grid_location[1] += 1
-      end
-      grid_location[1] = 1
-      grid_location[0] += 1
-      return nil if grid_location[0] > user_grid_select[0]
-      #next_square_y = starting_square_y.up
-
-      select_grid(user_grid_select,grid_location,starting_square_y.up,&block)
+    select_grid(user_grid_select, grid_location, starting_square_y.up, &block)
   end
 
-  def add_new_chesspiece(chesspiece_distination,chess_type)
-      select_grid(chesspiece_distination) do |square|
-          if chess_type == "♞"
-              square.data = Knight.new(chess_type)
-          else
-              square.data = Chesspiece.new(chess_type)
-          end
-          #return square.data
-      end
+  def add_new_chesspiece(chesspiece_distination, chess_type)
+    select_grid(chesspiece_distination) do |square|
+      square.data = if chess_type == '♞'
+                      Knight.new(chess_type)
+                    else
+                      Chesspiece.new(chess_type)
+                    end
+    end
   end
 end
