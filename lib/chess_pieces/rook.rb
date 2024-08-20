f5a86b76a4d09048f061c07f8518874422a3b142
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-08-20 15:17:49 +0700

    Add right moves generation for rook class

--- lib/chess_pieces/rook.rb
+++ lib/chess_pieces/rook.rb
@@ -6,11 +6,23 @@ class Rook < Chesspiece
   end
 
   def rook_move_check(chesspiece_location, chesspiece_distination)
-    p 'Ran'
+    p generate_moves(chesspiece_location)
   end
 
   def generate_moves(chesspiece_location)
     location = chesspiece_location.dup
+
+    ver_pos = location[0]
+    hor_pos = location[1]
+
+    possible_moves = []
+
+    (8 - location[1]).times do
+      hor_pos += 1
+      possible_moves << [ver_pos, hor_pos]
+    end
+
+    hor_pos = location[1]
 
     possible_moves
   end
