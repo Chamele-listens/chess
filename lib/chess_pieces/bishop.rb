a0383a218124a0e094f53fec28d3a73f41bf711e
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-08-22 15:37:04 +0700

    Add #generate_upper_right_moves

--- lib/chess_pieces/bishop.rb
+++ lib/chess_pieces/bishop.rb
@@ -7,11 +7,30 @@ class Bishop < Chesspiece
   end
 
   def bishop_move_check(chesspiece_location, chesspiece_distination, board)
-    generate_moves(chesspiece_location)
+    p generate_moves(chesspiece_location)
     p 'Ran!'
   end
 
   def generate_moves(chesspiece_location)
-    chesspiece_location
+    location = chesspiece_location.dup
+
+    ver_pos = location[0]
+    hor_pos = location[1]
+
+    possible_moves = []
+
+    generate_upper_right_moves(ver_pos, hor_pos, possible_moves, location)
+
+    possible_moves
+  end
+
+  def generate_upper_right_moves(ver_pos, hor_pos, possible_moves, location)
+    temp_moves = []
+    (8 - location[1]).times do
+      ver_pos += 1
+      hor_pos += 1
+      temp_moves << [ver_pos, hor_pos]
+    end
+    possible_moves << temp_moves
   end
 end
