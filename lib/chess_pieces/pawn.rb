c657a3f838839df79a677cf2d5c1b294ffa3ed4a
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-08-28 16:03:54 +0700

    Add #generate_one_or_two_up_moves to pawn

--- lib/chess_pieces/pawn.rb
+++ lib/chess_pieces/pawn.rb
@@ -3,17 +3,41 @@ require_relative 'chesspiece_main'
 # Check if pawn made legal moves change pawn to queen when at end of board
 class Pawn < Chesspiece
   include Basic_tools
+  attr_accessor :has_move
 
   def initialize(type)
     @type = type
     @has_move = false
   end
 
-  def pawn_move_check
-    generate_moves
+  def pawn_move_check(chesspiece_location, chesspiece_distination, board)
+    possible_moves = generate_moves(chesspiece_location)
+
+    return true if possible_moves.include?(chesspiece_distination)
+
+    false
   end
 
-  def generate_moves
-    p 'ran'
+  def generate_moves(chesspiece_location)
+    location = chesspiece_location.dup
+
+    ver_pos = location[0]
+    hor_pos = location[1]
+
+    possible_moves = []
+
+    generate_one_or_two_up_moves(possible_moves, ver_pos, hor_pos)
+
+    possible_moves
+  end
+
+  def generate_one_or_two_up_moves(possible_moves, ver_pos, hor_pos)
+    if @has_move == true
+      possible_moves << [ver_pos + 1, hor_pos]
+    else
+      possible_moves << [ver_pos + 1, hor_pos]
+      possible_moves << [ver_pos + 2, hor_pos]
+      @has_move = true
+    end
   end
 end
