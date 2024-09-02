2860ade2ec556bd0003c3cb64e8275ccc104bfe1
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-09-02 15:30:04 +0700

    Add queen chess piece

---
+++ lib/chess_pieces/queen.rb
@@ -0,0 +1,19 @@
+require_relative 'chesspiece_main'
+
+class Queen < chesspiece
+  include Basic_tools
+  attr_accessor :has_move
+
+  def initialize(type)
+    @type = type
+    @has_move = false
+  end
+
+  def queen_move_check(chesspiece_location,chesspiece_distination,board)
+    p 'ran'
+  end
+
+  def generate_moves(chesspiece_distination)
+    p 'ran'
+  end
+end
