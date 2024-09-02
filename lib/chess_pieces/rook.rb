0db2b4d2e725e92450b68cb61350168f1e39ed71
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-09-02 16:05:21 +0700

    Move rook moves to move_set module

--- lib/chess_pieces/rook.rb
+++ lib/chess_pieces/rook.rb
@@ -34,40 +34,4 @@ class Rook < Chesspiece
 
     possible_moves
   end
-
-  def generate_right_moves(possible_moves, ver_pos, hor_pos)
-    temp_moves = []
-    (8 - hor_pos).times do
-      hor_pos += 1
-      temp_moves << [ver_pos, hor_pos]
-    end
-    possible_moves << temp_moves
-  end
-
-  def generate_left_moves(possible_moves, ver_pos, hor_pos)
-    temp_moves = []
-    (hor_pos - 1).times do
-      hor_pos -= 1
-      temp_moves << [ver_pos, hor_pos]
-    end
-    possible_moves << temp_moves
-  end
-
-  def generate_up_moves(possible_moves, ver_pos, hor_pos)
-    temp_moves = []
-    (8 - ver_pos).times do
-      ver_pos += 1
-      temp_moves << [ver_pos, hor_pos]
-    end
-    possible_moves << temp_moves
-  end
-
-  def generate_down_moves(possible_moves, ver_pos, hor_pos)
-    temp_moves = []
-    (ver_pos - 1).times do
-      ver_pos -= 1
-      temp_moves << [ver_pos, hor_pos]
-    end
-    possible_moves << temp_moves
-  end
 end
