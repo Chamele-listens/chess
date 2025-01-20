5f6a0fa0dd990eb1fe208ad77277be40a9c69fff
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2025-01-20 16:36:30 +0700

    Add #write_to_save_file

--- lib/tools/save_load_system.rb
+++ lib/tools/save_load_system.rb
@@ -9,6 +9,13 @@ module Save_load_system
     new(data['board'], data['turn'])
   end
 
+  def write_to_save_file
+    save_data = save
+    out_file = File.new('lib/save_files/save_file.json', 'w')
+    out_file.puts(save_data)
+    out_file.close
+  end
+
   def create_save_file
     Dir.mkdir('lib/save_files') unless File.exist?('lib/save_files')
   end
