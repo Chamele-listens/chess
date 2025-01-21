a638655fcb8409bf1afb4c36a6831baa5b2ebff3
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2025-01-21 15:48:46 +0700

    Install oj gem and remove unnesscary code

--- lib/tools/save_load_system.rb
+++ lib/tools/save_load_system.rb
@@ -1,22 +1,20 @@
+require 'oj'
+
 # moldule for saving and loading files
 module Save_load_system
-  def save
-    JSON.dump({ board: @board, turn: @turn })
-  end
-
-  def self.load(save)
-    data = JSON.load save
-    new(data['board'], data['turn'])
-  end
-
-  def write_to_save_file
-    save_data = save
+  def write_to_save_file(game_object)
+    save_data = Oj.dump(game_object)
     out_file = File.new('lib/save_files/save_file.json', 'w')
     out_file.puts(save_data)
     out_file.close
   end
 
-  def create_save_file
+  def load_from_save_file
+    file_in = File.read('lib/save_files/save_file.json')
+    Oj.load(file_in)
+  end
+
+  def create_save_files_folder
     Dir.mkdir('lib/save_files') unless File.exist?('lib/save_files')
   end
 end
