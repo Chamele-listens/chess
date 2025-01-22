a6f912129b77a1c03b5c4a90a68fab4fc8cd23c8
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2025-01-22 14:40:04 +0700

    Move save_load? and others to save_load_system.rb

--- lib/tools/save_load_system.rb
+++ lib/tools/save_load_system.rb
@@ -17,4 +17,47 @@ module Save_load_system
   def create_save_files_folder
     Dir.mkdir('lib/save_files') unless File.exist?('lib/save_files')
   end
+
+  def save_load?(player_input, game_object)
+    return false if player_input.count('a-zA-Z') <= 0
+
+    create_save_files_folder
+
+    if player_input == 'save'
+      return true if save_file_override? == false
+
+      save(game_object)
+    elsif player_input == 'load'
+      load
+    else
+      p 'not vaild input'
+    end
+
+    true
+  end
+
+  def save(game_object)
+    write_to_save_file(game_object)
+    p 'Game has been saved'
+  end
+
+  def save_file_override?
+    return unless File.exist?('lib/save_files/save_file.json')
+
+    p 'Do you want to override your save file? (y/n)'
+    input = gets.chomp.downcase
+
+    return true if %w[y yes].include?(input)
+
+    false
+  end
+
+  def load
+    return p 'There is no save file' unless File.exist?('lib/save_files/save_file.json')
+
+    save_data = load_from_save_file
+    @board = save_data.board
+    @turn = save_data.turn
+    p 'Game has been loaded'
+  end
 end
