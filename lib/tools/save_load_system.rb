e9875f10e19fa0e6c55c4057989d03bad5eaf8b8
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2025-01-30 12:29:46 +0700

    Add #check_os_for_save_file_creation

--- lib/tools/save_load_system.rb
+++ lib/tools/save_load_system.rb
@@ -11,6 +11,14 @@ module Save_load_system
     end
   end
 
+  def check_os_for_save_file_creation
+    if OS.linux? || OS.mac?
+      'lib/save_files'
+    elsif OS.windows?
+      'lib\save_files'
+    end
+  end
+
   def write_to_save_file(game_object)
     save_data = Oj.dump(game_object)
     out_file = File.new('lib/save_files/save_file.json', 'w')
